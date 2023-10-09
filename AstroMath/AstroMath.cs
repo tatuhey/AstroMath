b60e69aeba4fef95bbbcbd71c4dbc8cf20aeab34
RAIHAN ABDILLAH <30065695@tafe.wa.edu.au>
2023-10-09 12:34:19 +0800

    Comments

--- AstroMath/AstroMath.cs
+++ AstroMath/AstroMath.cs
@@ -8,8 +8,10 @@ namespace AstroMath
 {
     public class AstroMath
     {
-        // NOT ERROR TRAPPED YET
-
+        // Star Velocity: Create a method to measure the Star velocity using the Doppler shift,
+        // it should have two input parameters of type double (Observed Wavelength and Rest Wavelength)
+        // and return a double which represents the velocity.
+        // The Doppler shift of a star can be measured by using the change in wavelength of that object. 
         public static double StarVelocity (double ObsWavelength, double RestWavelength)
         {
             double SpeedOfLight = 299792458;
@@ -18,6 +20,10 @@ namespace AstroMath
             return Math.Round(Velocity, 2);
         }
 
+        // Star Distance: Create a method to measure the star distance using the parallax angle,
+        // it should have a single input parameter of type double (Arcseconds angle) and return a double.
+        // The parallax angle is measured at two different points and works on nearby stars.
+        // The method must return a double which is a value in parsecs.
         public static double StarDistance(double Parallax)
         {
             if (Parallax == 0)
@@ -28,7 +34,9 @@ namespace AstroMath
             return Math.Round((1 / Parallax), 2);
         }
 
-
+        // Temperature in Kelvin: The Kelvin temperature scale is the primary temperature used in science
+        // and is easily converted from Celsius. Create a method that has a single input parameter of type double
+        // (temperature in Celsius) and returns a double which is the temperature in degrees kelvin. 
         public static double TempInKelvin (double Celcius)
         {
             if (Celcius < -273)
@@ -39,6 +47,10 @@ namespace AstroMath
             return Math.Round((Celcius + 273), 2);
         }
 
+        // Event Horizon (Schwarzschild Radius): Create a method that will return the distance
+        // from the centre of a blackhole to the event horizon.
+        // The method must have a single input parameter of type double (Blackhole Mass)
+        // and return a double which is the event horizon in metres
         public static double EventHorizon (double BlackholeMass)
         {
             double GravityConst = 6.674e-11;
